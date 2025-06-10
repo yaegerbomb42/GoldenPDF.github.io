9a5b91729eff3352740e1fe58f1ab1e1b603b7f2
james yaeger <yaeger.james42@gmail.com>
2025-06-09 23:09:41 -0500

    new

--- .history/vite.config_20250609230520.ts
+++ .history/vite.config_20250609230520.ts
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
